f245cc2622053c6c77df0d1487b63c32a4cbd232
gabreb <62906858+gabreb@users.noreply.github.com>
2020-04-22 13:18:06 -0700

    hi

--- src/_10_cuteness_tv/cutenessTV.java
+++ src/_10_cuteness_tv/cutenessTV.java
@@ -2,6 +2,7 @@ package _10_cuteness_tv;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,21 +19,58 @@ public void run() {
 	JPanel panel = new JPanel();
 	frame.add(panel);
 	panel.add(button);
-	button.setText("B1");
+	button.setText("Ducks");
 	frame.pack();
 	button.addActionListener(this);
 	panel.add(button2);
-	button2.setText("B2");
+	button2.setText("Frogs");
 	frame.pack();
 	button2.addActionListener(this);
 	panel.add(button3);
-	button3.setText("B3");
+	button3.setText("FluffyUnicrons");
 	frame.pack();
 	button3.addActionListener(this);
 }
 @Override
-public void actionPerformed(ActionEvent e) {
-	
-	
+public void actionPerformed(ActionEvent arg0) {
+	JButton buttonPressed = (JButton) arg0.getSource();
+	if (buttonPressed==button) {
+		showDucks();
+	}
+	else if (buttonPressed==button2) {
+		showFrog();
+	}
+	else if (buttonPressed==button3) {
+		showFluffyUnicorns(); 
+	}
+}
+void showDucks() {
+    playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+}
+
+void showFrog() {
+    playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+}
+
+void showFluffyUnicorns() {
+    playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+}
+
+void playVideo(String videoID) {
+    
+    // Workaround for Linux because "Desktop.getDesktop().browse()" doesn't 
+    //work on some Linux implementations 
+    try {
+    if (System.getProperty("os.name").toLowerCase().contains("linux")) {
+    if (Runtime.getRuntime().exec(new String[] { 
+     "which", "xdg- open" }).getInputStream().read() != -1) {
+    Runtime.getRuntime().exec(new String[] { "xdg-open", videoID }); 
+    }
+    } else {
+         URI uri = new URI(videoID);
+         java.awt.Desktop.getDesktop().browse(uri);
+    }      } catch (Exception e) {
+         e.printStackTrace();
+    }
 }
 }
